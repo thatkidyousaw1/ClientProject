8f1c7737ec4444250fac2cd5d1775dc93ab4a050
unknown <thatkidyousaw1@gmail.com>
2023-02-10 19:59:52 +0800

    oten

---
+++ ClientProject/DatabaseService/DataContext.cs
@@ -0,0 +1,39 @@
+﻿using ClientProject.Model;
+using System;
+using System.Collections.Generic;
+//using System.Data.Entity.ModelConfiguration.Conventions;
+//using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClientProject.DatabaseService
+{
+    public class DataContext : DbContext
+    {
+        //public DataContext() : base(new SQLiteConnection()
+        //{
+        //    ConnectionString = new SQLiteConnectionStringBuilder()
+        //    {
+        //        DataSource = "clientdatabase.db",
+        //        ForeignKeys = false
+        //    }.ConnectionString
+        //}, true)
+        //{ }
+
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        //    base.OnModelCreating(modelBuilder);
+        //}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = clientdatabase.db");
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ ClientProject/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using ClientProject.DatabaseService;
+using ClientProject.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public List<UserData> DatabaseUsers { get; private set; }
+
+        public void Create()
+        {
+            using (InputDataContext context = new InputDataContext())
+            {
+                var client = ClientName.Text;
+                var department = DepartmentName.Text;
+                var purchaseOrder = PurchaseOrder.Text;
+                var tinNo = TinNumber.Text;
+                var address = Address.Text;
+                var type = TypeName.Text;
+                var quantity = Quantity.Text;
+                var items = Items.Text;
+                var unitPrice = UnitPrice.Text;
+                var amount = Amount.Text;
+                var checkAmount = CheckAmount.Text;
+                var withTax = WithholdingTax.Text;
+                var officeReceiptNo = ReceiptNumber.Text;
+                var datePaid = DatePaid.Text;
+                var bir2307 = Bir2307.Text;
+                var bir2306 = Bir2306.Text;
+
+                if (client != null && department != null && purchaseOrder != null && tinNo != null
+                    && address != null && type != null && quantity != null && items != null && unitPrice != null && 
+                    amount != null && checkAmount != null && withTax != null &&  officeReceiptNo != null && 
+                    datePaid != null && bir2307 != null && bir2306 != null)
+                {
+                    context.UserData.Add(new UserData()
+                    {
+                        ClientName = client,
+                        Department = department,
+                        PurchaseOrder = purchaseOrder,
+                        TinNumber = tinNo,
+                        Address = address,
+                        Type = type,
+                        Quantity = quantity,
+                        Items = items,
+                        UnitPrice = unitPrice,
+                        Amount = amount,
+                        CheckAmount = checkAmount,
+                        WithTax = withTax,
+                        OfficeReceiptNumber = officeReceiptNo,
+                        DatePaid = datePaid,
+                        Bir2306 = bir2306,
+                        Bir2307 = bir2307,
+                    });
+
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public void Update()
+        {
+            using (InputDataContext context = new InputDataContext())
+            {
+                DatabaseUsers = context.UserData.ToList();
+                ProductListView.ItemsSource = DatabaseUsers;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Create();
+            Update();
+        }
+    }
+}
